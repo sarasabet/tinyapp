3b7f925818f8c87a78812e7a7b61c677017cb340
Sara Sabet <sara.ilbeigi@gmail.com>
2021-08-30 17:39:21 +0000

    add a get rout to send JSON format of db

--- express_server.js
+++ express_server.js
@@ -4,12 +4,18 @@ const PORT = 8080; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
+
 
 app.get("/", (req, res) => {
   res.send("Helloooooo!");
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
